206e85adf9d7b235df0e9a264d09559d19fe015f
Pablo Margreff <pmargreff@gmail.com>
2017-05-11 21:52:50 -0300

    creating simple test skeleton

---
+++ btree/btree_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestInsert(t *testing.T) {
+}
+
+func TestSearch(t *testing.T) {
+}
+
+func TestRemove(t *testing.T) {
+}
+
+func TestBalance(t *testing.T) {
+}
