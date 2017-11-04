ca8153f0cad1ab6708aa4c56c35c84c22aad5403
Pablo Margreff <pmargreff@gmail.com>
2017-11-04 17:03:25 -0200

    allow to atribute with direct array index

--- hash/hash.go
+++ hash/hash.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
-const LOAD_FACTOR int = 4
+const GLOBAL_DEPTH int = 4
 const INITIAL_SIZE int = 100
 type bucket struct {
   key string
 }
 
 type hash struct {
-  size int
   buckets [INITIAL_SIZE]*bucket
+  filledBuckets int
+  loadFactor float64
+  size int
 }
 
 func main()  {
@@ -18,5 +20,11 @@ func main()  {
 }
 
 func CreateHash() *hash{
-  return &hash{size: INITIAL_SIZE}
+  return &hash{size: INITIAL_SIZE, filledBuckets: 0, loadFactor: 0.0}
+}
+
+func AddBucket(hash *hash, new_bucket bucket) {
+  hash.filledBuckets++;
+  hash.loadFactor = (float64)(hash.size / hash.filledBuckets)
+  hash.buckets[2] = &bucket{key: new_bucket.key}
 }
--- hash/hash_test.go
+++ hash/hash_test.go
@@ -8,4 +8,13 @@ import (
 func TestCreateHash(t *testing.T) {
   hash := CreateHash()
   assert.Equal(t, hash.size, INITIAL_SIZE)
+  assert.Equal(t, hash.filledBuckets, 0)
+  assert.Equal(t, hash.loadFactor, 0.0)
+}
+
+func TestAddBucket(t *testing.T)  {
+  hash := CreateHash()
+  AddBucket(hash, bucket{"abc"})
+  assert.Equal(t, 1, hash.filledBuckets)
+  assert.Equal(t, "abc", hash.buckets[2].key)
 }
